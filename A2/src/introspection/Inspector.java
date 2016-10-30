d2af73c1c07be539649b6bca731d58ff89b0d09a
msimiste <mikesimister@shaw.ca>
2016-10-29 20:42:22 -0600

    Methods and associated values functioning

--- A2/src/introspection/Inspector.java
+++ A2/src/introspection/Inspector.java
@@ -8,6 +8,9 @@ public class Inspector {
 	private Map<Integer, Class<?>> classObjects;
 	private int idCounter = 0;
 
+	public final String IDIVIDER = "-------------------------------------------";
+	public final String ODIVIDER = "*******************************************";
+
 	public void inspect(Object obj, boolean recursive) {
 
 		if (obj == null) {
@@ -29,14 +32,61 @@ public class Inspector {
 		System.out.println("Immediate Superclass: "
 				+ cls.getSuperclass().getName());
 		Class<?>[] iFaces = cls.getInterfaces();
-
+		System.out.println(IDIVIDER);
 		int total = iFaces.length;
 		int count = 1;
 		System.out.println("This class implements " + total + " interfaces");
 		for (Class<?> c : iFaces) {
 			System.out.println("\tInterface  " + (count++) + " " + c.getName());
 		}
+		System.out.println(IDIVIDER);
+		Method[] methods = cls.getMethods();
+		int methodTotal = methods.length;
+		System.out.println("This class declares " + methodTotal + " Methods");
+		int methodCount = 1;
+		for (Method m : methods) {
+			System.out.println(ODIVIDER);
+			System.out.println("Method " + (methodCount++) + " is "
+					+ m.getName());
+			System.out.println(ODIVIDER);
+			System.out.println("\t"+m.getName()+ " returns an object of type " + m.getReturnType().getName());
+			System.out.println(IDIVIDER);
+			System.out.println("\t" + m.getName() + " throws "
+					+ m.getExceptionTypes().length + " excptions");
+			listExceptions(m);
+			System.out.println("\t" + m.getName() + " requires "
+					+ m.getParameterCount() + " parameters");
+			listParameterTypes(m);
+			System.out.print("\t" + m.getName() + " has the following modifiers: ");
+			listModifiers(m);
 
+		}
+	}
+
+	private void listExceptions(Method m) {		
+		Class<?>[] exceptions = m.getExceptionTypes();
+		int count = 1;
+		for (Class<?> e : exceptions) {
+			System.out.println("\t\t" + e.getName());
+		}
+		System.out.println(IDIVIDER);
+	}
+
+	private void listParameterTypes(Method m) {
+		Class<?>[] parameters = m.getParameterTypes();
+		Parameter[] params = m.getParameters();
+		int count = 1;
+		for (Class<?> p : parameters) {
+			System.out.println("\t" + params[(count++) - 1].getName() + "\t"
+					+ p.getName());
+		}
+		System.out.println(IDIVIDER);
+	}
+	
+	private void listModifiers(Method m ){
+		System.out.println(Modifier.toString(m.getModifiers()));
+		System.out.println(IDIVIDER);
+		
 	}
 
 }
