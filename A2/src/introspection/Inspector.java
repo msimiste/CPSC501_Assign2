a610ad890bdcdcd0242085f11ecfde34186e3c49
msimiste <mikesimister@shaw.ca>
2016-10-29 18:45:29 -0600

    get interfaces is functioning

--- A2/src/introspection/Inspector.java
+++ A2/src/introspection/Inspector.java
@@ -16,16 +16,27 @@ public class Inspector {
 
 		classObjects = new HashMap<Integer, Class<?>>();
 		classObjects.put(idCounter++, obj.getClass());
-		
-		for(Class<?> cls : classObjects.values()){
+
+		for (Class<?> cls : classObjects.values()) {
 			getClassInfo(cls);
 		}
 	}
 
 	private void getClassInfo(Class<?> cls) {
-		
 		System.out.println("Class: " + cls.getName());
-		System.out.println("Superclass: " + cls.getSuperclass().getName());
-		
+		System.out.println("Class simple: " + cls.getSimpleName());
+		System.out.println("Class caonical: " + cls.getCanonicalName());
+		System.out.println("Immediate Superclass: "
+				+ cls.getSuperclass().getName());
+		Class<?>[] iFaces = cls.getInterfaces();
+
+		int total = iFaces.length;
+		int count = 1;
+		System.out.println("This class implements " + total + " interfaces");
+		for (Class<?> c : iFaces) {
+			System.out.println("\tInterface  " + (count++) + " " + c.getName());
+		}
+
 	}
+
 }
