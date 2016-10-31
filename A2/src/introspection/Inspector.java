e2276a1bc7006c7bebe7ae6554be5aea93785a71
Mike Simister <mikesimister@shaw.ca>
2016-10-31 13:27:10 -0600

    Refactoring, extract method within getClassInfo()

--- A2/src/introspection/Inspector.java
+++ A2/src/introspection/Inspector.java
@@ -62,79 +62,9 @@ public class Inspector {
 	
 		displayClassInterfaces(cls);
 		displayFields(cls);
-
-		/*Field[] fields = cls.getDeclaredFields();
-		int fieldTotal = fields.length;
-		int fieldCount = 1;
-		System.out.println(ODIVIDER);
-		System.out.println(cls.getName() + " declares " + fieldTotal
-				+ " Fields");
-		System.out.println(IDIVIDER);
-		for (Field f : fields) {
-
-			f.setAccessible(true);
-			System.out.println("Field " + (fieldCount));
-			System.out.println("\tName: " + f.getName());
-
-			try {
-				Class<?> type = f.getType();
-				if (type.isPrimitive()) {
-					Object c = f.get(cls.newInstance());
-					System.out.println("\tType: " + c.getClass().getTypeName());
-					System.out.println("\tValue: " + c.toString());
-					System.out.println("\tModifiers: "
-							+ Modifier.toString(c.getClass().getModifiers()));
-				} else if (type.isArray()) {
-					int indexLength = Array.getLength(f.get(cls.newInstance()));
-
-					Object[] arr = (Object[]) Array.newInstance(type,
-							indexLength);
-					System.out.println("\tType: "
-							+ arr.getClass().getTypeName());
-					System.out.println("\tLength: " + arr.length);
-					for (int i = 0; i < arr.length; i++) {
-						if (arr[i] == null) {
-							System.out.println("\t\tValue: null");
-						} else {
-							System.out.println("\tValue: "
-									+ arr[i].getClass().getName());
-						}
-					}
-					System.out.println("\tModifiers: "
-							+ Modifier.toString(arr.getClass().getModifiers()));
-				} else {
-					Object c;
-					if (!(recursive)) {
-						c = f.get(cls.newInstance());
-					} else {
-						c = f.getClass();
-					}
-					System.out.print("\tType: " + type);
-					if (c != null) {
-						System.out.println("\tValue: " + c.toString());
-					} else {
-						System.out.println("\tValue: null");
-					}
-				}
-				fieldCount++;
-				System.out.println(IDIVIDER);
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-*/
+		displayConstructors(cls);
 		
-		Constructor<?>[] constructors = cls.getDeclaredConstructors();
+	/*	Constructor<?>[] constructors = cls.getDeclaredConstructors();
 		int constructorTotal = constructors.length;
 		int constructorCount = 1;
 		System.out.println(cls.getName() + " has " + constructorTotal
@@ -153,7 +83,7 @@ public class Inspector {
 			listConstructorModifiers(c);
 			System.out.println(IDIVIDER);
 		}
-
+*/
 		Method[] methods = cls.getDeclaredMethods();
 		int methodTotal = methods.length;
 		System.out.println(ODIVIDER);
@@ -179,6 +109,28 @@ public class Inspector {
 			System.out.println(IDIVIDER);
 		}
 
+	}
+	
+	private void displayConstructors(Class<?> cls){
+		Constructor<?>[] constructors = cls.getDeclaredConstructors();
+		int constructorTotal = constructors.length;
+		int constructorCount = 1;
+		System.out.println(cls.getName() + " has " + constructorTotal
+				+ " constructors ");
+		System.out.println(ODIVIDER);
+		for (Constructor<?> c : constructors) {
+			System.out.println(IDIVIDER);
+			System.out.println("Constructor " + (constructorCount++) + " is "
+					+ c.getName());
+			System.out.println(IDIVIDER);
+			System.out.println("\t" + c.getName() + " requires: "
+					+ c.getParameterCount() + " parameters");
+			listConstructorParameterTypes(c);
+			System.out.print("\t" + c.getName()
+					+ " has the following modifiers: ");
+			listConstructorModifiers(c);
+			System.out.println(IDIVIDER);
+		}
 	}
 	
 	private void displayClassInterfaces(Class<?> cls){
